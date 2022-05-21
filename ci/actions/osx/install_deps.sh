cb5e6329c7004a5bfeaad7d340f20f6c0ac3f2ed
Fiono11 <ruipedromorais11@gmail.com>
2022-05-21 19:18:02 +0100

    minor fixes

--- ci/actions/osx/install_deps.sh
+++ ci/actions/osx/install_deps.sh
@@ -3,10 +3,10 @@
 brew update
 brew install coreutils
 brew install rust
-brew cask install xquartz
+brew install --cask xquartz
 sudo util/build_prep/fetch_boost.sh
 util/build_prep/macosx/build_qt.sh
 git clone https://github.com/corrosion-rs/corrosion.git
 cmake -Scorrosion -Bbuild -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
-cmake --install build --config Release
+sudo cmake --install build --config Release
