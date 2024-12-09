a7da6d14823cbf53732ff0d4c8b5042749ec591a
Gustav Schauwecker <service@simpago.com>
2024-12-09 15:38:33 +0100

    Read bootstrap weights from text format

--- node/src/consensus/bootstrap_weights.rs
+++ node/src/consensus/bootstrap_weights.rs
@@ -1,39 +1,36 @@
-use rsnano_core::{
-    utils::{BufferReader, Deserialize, StreamExt},
-    Account, Amount, Networks, PublicKey,
-};
+use rsnano_core::{Account, Amount, Networks, PublicKey};
 use rsnano_ledger::RepWeightCache;
 use std::collections::HashMap;
 use tracing::info;
 
 pub(crate) fn get_bootstrap_weights(network: Networks) -> (u64, HashMap<PublicKey, Amount>) {
-    let buffer = get_bootstrap_weights_bin(network);
+    let buffer = get_bootstrap_weights_text(network);
     deserialize_bootstrap_weights(buffer)
 }
 
-fn get_bootstrap_weights_bin(network: Networks) -> &'static [u8] {
+fn get_bootstrap_weights_text(network: Networks) -> &'static str {
     if network == Networks::NanoLiveNetwork {
-        include_bytes!("../../rep_weights_live.bin")
+        include_str!("../../rep_weights_live.txt")
     } else {
-        include_bytes!("../../rep_weights_beta.bin")
+        include_str!("../../rep_weights_beta.txt")
     }
 }
 
-fn deserialize_bootstrap_weights(buffer: &[u8]) -> (u64, HashMap<PublicKey, Amount>) {
-    let mut reader = BufferReader::new(buffer);
+fn deserialize_bootstrap_weights(buffer: &str) -> (u64, HashMap<PublicKey, Amount>) {
     let mut weights = HashMap::new();
+    let mut first_line = true;
     let mut max_blocks = 0;
-    if let Ok(count) = reader.read_u128_be() {
-        max_blocks = count as u64;
-        loop {
-            let Ok(account) = PublicKey::deserialize(&mut reader) else {
-                break;
-            };
-            let Ok(weight) = Amount::deserialize(&mut reader) else {
-                break;
-            };
-            weights.insert(account, weight);
+    for line in buffer.lines() {
+        if first_line {
+            max_blocks = line.parse().unwrap();
+            first_line = false;
+            continue;
         }
+
+        let mut it = line.split(':');
+        let account = Account::decode_account(it.next().unwrap()).unwrap();
+        let weight = Amount::decode_dec(it.next().unwrap()).unwrap();
+        weights.insert(account.into(), weight);
     }
 
     (max_blocks, weights)
@@ -73,15 +70,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn bootstrap_weights_bin() {
+    fn bootstrap_weights_text() {
         assert_eq!(
-            get_bootstrap_weights_bin(Networks::NanoLiveNetwork).len(),
-            6400,
+            get_bootstrap_weights_text(Networks::NanoLiveNetwork).len(),
+            13921,
             "expected live weights don't match'"
         );
         assert_eq!(
-            get_bootstrap_weights_bin(Networks::NanoBetaNetwork).len(),
-            0,
+            get_bootstrap_weights_text(Networks::NanoBetaNetwork).len(),
+            1161,
             "expected beta weights don't match'"
         );
     }
@@ -89,7 +86,7 @@ mod tests {
     #[test]
     fn bootstrap_weights() {
         let (max_blocks, weights) = get_bootstrap_weights(Networks::NanoLiveNetwork);
-        assert_eq!(weights.len(), 133);
-        assert_eq!(max_blocks, 201_514_829);
+        assert_eq!(weights.len(), 135);
+        assert_eq!(max_blocks, 204_137_485);
     }
 }
